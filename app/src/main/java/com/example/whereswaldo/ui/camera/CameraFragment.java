ad39a73f8f55de323ec15a506c15bf87d0cf35f2
zenzen26 <thamzien@gmail.com>
2023-05-27 02:52:55 +1000

    New version without classifyin image

---
+++ app/src/main/java/com/example/whereswaldo/ui/camera/CameraFragment.java
@@ -0,0 +1,123 @@
+package com.example.whereswaldo.ui.camera;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.fitness.data.DataType;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.example.whereswaldo.R;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CameraFragment extends Fragment {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 3;
+    private static final int REQUEST_IMAGE_PICK = 1;
+
+    private Button cameraButton;
+    private Button galleryButton;
+    private ImageView imageView;
+    private TextView resultTextView;
+    private int imageSize = 32;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        cameraButton = view.findViewById(R.id.button);
+        galleryButton = view.findViewById(R.id.button2);
+        imageView = view.findViewById(R.id.imageView);
+        resultTextView = view.findViewById(R.id.result);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                }
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        return view;
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_PICK);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+//                classifyImage(image);
+            } else if (requestCode == REQUEST_IMAGE_PICK && data != null) {
+                Uri selectedImageUri = data.getData();
+                Bitmap image = null;
+                try {
+                    image = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), selectedImageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+//                classifyImage(image);
+            }
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/whereswaldo/ui/map/MapFragment.java
@@ -0,0 +1,158 @@
+package com.example.whereswaldo.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.whereswaldo.R;
+import com.example.whereswaldo.databinding.FragmentNotificationsBinding;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MapFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    GoogleMap mGoogleMap;
+    DatabaseReference coordinatesRef; // Firebase reference
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        // Get the Firebase coordinates reference
+        coordinatesRef = FirebaseDatabase.getInstance().getReference();
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                MapsInitializer.initialize(getContext());
+                mGoogleMap = googleMap;
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                // Fetch coordinates from Firebase and add markers on the map
+                coordinatesRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        googleMap.clear(); // Clear existing markers
+                        LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            double latitude = snapshot.child("Latitude").getValue(Double.class);
+                            double longitude = snapshot.child("Longitude").getValue(Double.class);
+                            String title = snapshot.child("Name").getValue(String.class);
+                            LatLng position = new LatLng(latitude, longitude);
+
+                            googleMap.addMarker(new MarkerOptions().position(position).title(title));
+                            boundsBuilder.include(position);
+                        }
+
+                        LatLngBounds bounds = boundsBuilder.build();
+                        int padding = 50; // Adjust the padding as needed
+
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                        googleMap.animateCamera(cameraUpdate);
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle error if needed
+                    }
+                });
+
+                // Set marker click listener
+                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(@NonNull Marker marker) {
+                        LatLng position = marker.getPosition();
+                        double latitude = position.latitude;
+                        double longitude = position.longitude;
+
+                        // Query the Firebase database based on the latitude and longitude
+                        DatabaseReference coordinatesRef = FirebaseDatabase.getInstance().getReference();
+                        Query query = coordinatesRef.orderByChild("Latitude").equalTo(latitude);
+                        query.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    String location = snapshot.child("Location").getValue(String.class);
+                                    String name = snapshot.child("Name").getValue(String.class);
+                                    String details = snapshot.child("Details").getValue(String.class);
+                                    String artist = snapshot.child("Artist").getValue(String.class);
+
+                                    // Create a BottomSheetDialog
+                                    BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext());
+
+                                    // Inflate the layout for the bottom sheet
+                                    View bottomSheetView = getLayoutInflater().inflate(R.layout.dialog_landmark, null);
+
+                                    // Set the title of the location in the TextView
+                                    TextView locationNameTextView = bottomSheetView.findViewById(R.id.location_name);
+                                    locationNameTextView.setText(name);
+
+                                    // Set the location, artist, and details in the TextViews
+                                    TextView locationTextView = bottomSheetView.findViewById(R.id.location);
+                                    locationTextView.setText(location != null ? location : "");
+
+                                    TextView artistTextView = bottomSheetView.findViewById(R.id.artist);
+                                    artistTextView.setText(artist); // Set the artist value if applicable
+
+                                    TextView detailsTextView = bottomSheetView.findViewById(R.id.details);
+                                    detailsTextView.setText(details != null ? details : "");
+
+                                    // Set the view of the BottomSheetDialog
+                                    bottomSheetDialog.setContentView(bottomSheetView);
+
+                                    // Show the BottomSheetDialog
+                                    bottomSheetDialog.show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                // Handle the error
+                            }
+                        });
+
+                        return true;
+                    }
+                });
+
+
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
